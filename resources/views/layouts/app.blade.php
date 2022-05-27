6358911237ba69fd661dcfede3ddf872d1c4ae6e
Ángel Alonso <angelai05@educastur.es>
2022-05-27 10:44:06 +0200

    Modificado el menú, movido el enlace del carrito

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,16 +57,6 @@
                                 {{ __("Panel administrador") }}
                         @endif
 
-                        <a href="{{ route('cart.list') }}" class="btn menuGeneral text-white text-lg text-gray-100 no-underline px-5">
-                            <div class="flex">
-                                <svg class="w-5 h-5 mt-1" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                                </svg>
-
-                                <div>{{ Cart::getTotalQuantity()}}</div>
-                            </div>
-                            
-                        </a>
 
                     @endauth
 
@@ -74,23 +64,36 @@
 
                 <!-- LOGIN, REGISTER Y LOGOUT -->
                 <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
-                    @guest
-                        <a class="no-underline hover:underline text-white btn btn-primary" href="{{ route('login') }}">{{ __('Acceder') }}</a>
-                        @if (Route::has('register'))
-                            <a class="no-underline hover:underline btn btn-primary" href="{{ route('register') }}">{{ __('Regístrate') }}</a>
-                        @endif
-                    @else
-                        <span class="text-white">{{ Auth::user()->name }}</span>
+                @guest
+                    <a class="no-underline hover:underline text-white btn btn-primary" href="{{ route('login') }}">{{ __('Acceder') }}</a>
+                    @if (Route::has('register'))
+                        <a class="no-underline hover:underline btn btn-primary" href="{{ route('register') }}">{{ __('Regístrate') }}</a>
+                    @endif
+                @else
+                    <a href="{{ route('cart.list') }}" class="btn menuGeneral text-white text-lg text-gray-100 no-underline">
+                        <div class="flex">
+                            <svg class="w-5 h-5 mt-1" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                                <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                            </svg>
 
-                        <a href="{{ route('logout') }}"
-                           class="btn btn-danger text-white no-underline hover:underline"
-                           onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">{{ __('Desconectarse') }}</a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
-                            {{ csrf_field() }}
-                        </form>
-                    @endguest
-                </nav>
+                            <div>{{ Cart::getTotalQuantity()}}</div>
+                        </div> 
+                    </a>
+
+                    <span class="text-white">{{ Auth::user()->name }}</span>
+
+                    <a href="{{ route('logout') }}"
+                    class="btn btn-danger text-white no-underline hover:underline"
+                    onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="25" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                                                                                <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+                                                                                <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                                                                             </svg></a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                        {{ csrf_field() }}
+                    </form>
+                @endguest
+            </nav>
             </div>
             <!-- FIN MENÚ -->
         </header>
